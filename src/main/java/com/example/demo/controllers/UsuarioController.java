d47d9c563b2f313085802fd529844ce93420961d
sam <samueldazadaza@gmail.com>
2022-10-05 18:52:09 -0500

    se agrega CORS al controlador para acceder al back

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.example.demo.models.UsuarioModel;
 import com.example.demo.services.UsuarioService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired
